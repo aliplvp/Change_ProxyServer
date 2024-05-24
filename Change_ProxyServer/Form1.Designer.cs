4f5404132c0e723e011145016f0b96241dbfeddf
ssn577 <shalip0919@gmail.com>
2024-05-24 23:38:18 +0330

    Add project files.

---
+++ Change_ProxyServer/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace Change_ProxyServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.txt_Port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_set = new System.Windows.Forms.Button();
+            this.btn_off = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(57, 6);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(100, 20);
+            this.txt_IP.TabIndex = 0;
+            this.txt_IP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_Port
+            // 
+            this.txt_Port.Location = new System.Drawing.Point(163, 6);
+            this.txt_Port.Name = "txt_Port";
+            this.txt_Port.Size = new System.Drawing.Size(40, 20);
+            this.txt_Port.TabIndex = 1;
+            this.txt_Port.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP:Port";
+            // 
+            // btn_set
+            // 
+            this.btn_set.Location = new System.Drawing.Point(209, 6);
+            this.btn_set.Name = "btn_set";
+            this.btn_set.Size = new System.Drawing.Size(54, 20);
+            this.btn_set.TabIndex = 3;
+            this.btn_set.Text = "Set";
+            this.btn_set.UseVisualStyleBackColor = true;
+            this.btn_set.Click += new System.EventHandler(this.btn_set_Click);
+            // 
+            // btn_off
+            // 
+            this.btn_off.Location = new System.Drawing.Point(269, 6);
+            this.btn_off.Name = "btn_off";
+            this.btn_off.Size = new System.Drawing.Size(54, 20);
+            this.btn_off.TabIndex = 4;
+            this.btn_off.Text = "Off";
+            this.btn_off.UseVisualStyleBackColor = true;
+            this.btn_off.Click += new System.EventHandler(this.btn_off_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(12, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Creator : AliPlvP.iR";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 54);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_off);
+            this.Controls.Add(this.btn_set);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Port);
+            this.Controls.Add(this.txt_IP);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change ProxyServer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_IP;
+        private System.Windows.Forms.TextBox txt_Port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_set;
+        private System.Windows.Forms.Button btn_off;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Change_ProxyServer/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Change_ProxyServer
+{
+    public partial class Form1 : Form
+    {
+        RegistryKey Internet_Settings = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_set_Click(object sender, EventArgs e)
+        {
+            string proxy = txt_IP.Text + ":" + txt_Port.Text;
+            Internet_Settings.SetValue("ProxyEnable",1);
+            Internet_Settings.SetValue("ProxyServer",proxy);
+            this.Text = "Change To " + proxy;
+            System.Threading.Thread.Sleep(1000);
+            Application.Exit();
+        }
+
+        private void btn_off_Click(object sender, EventArgs e)
+        {
+            Internet_Settings.SetValue("ProxyEnable", 0);
+            this.Text = "Proxy Off...";
+            System.Threading.Thread.Sleep(1000);
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ip = Internet_Settings.GetValue("ProxyServer").ToString();
+            var new_ip = ip.Replace("http://", "");
+            var parts = new_ip.Split(':');
+
+            string ipAddress = parts[0];
+            string port = parts[1];
+
+            txt_IP.Text = ipAddress;
+            txt_Port.Text = port;
+
+            if ((int)Internet_Settings.GetValue("ProxyEnable", 1) == 1)
+            {
+                btn_off.Enabled = true;
+                btn_set.Enabled = false;
+            }
+            else
+            {
+                btn_off.Enabled = false;
+                btn_set.Enabled = true;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.aliplvp.ir/");
+        }
+    }
+}
